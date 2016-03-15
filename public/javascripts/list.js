e032232e2c8bd727c95853795e40844edaa83c71
rahulforallp <rahulforallp@gmail.com>
2016-03-16 00:18:33 +0530

    insert

--- public/javascripts/list.js
+++ public/javascripts/list.js
@@ -1,6 +1,22 @@
 
      var internApp = angular.module('internApp', []);
 
+              internApp.controller('AddCtrl', function ($scope, $http){
+              alert("hello")
+                     $scope.save = function(){
+                     var data1 = {"id":8,"name":$scope.name,"email":$scope.email,"mobile":$scope.mobile,"address":$scope.address,"emergency":$scope.emergency};
+
+                              $http({
+                                  method:'POST',
+                                  url:'/addNew',
+                                  data: JSON.stringify(data1),
+                                  contentType: 'application/json',
+                                  dataType:'json'
+                              })
+                     }})
+
+
+
              internApp.controller('InternCtrl', function ($scope, $http){
 
                $http.get('/list').success(function(data) {
@@ -8,17 +24,6 @@
                });
              });
 
-        internApp.controller('AddCtrl', function ($scope, $http){
-        $scope.save = function(){
-        var data1 = {"id":1,"name":$scope.name,"email":$scope.email,"mobile":$scope.mobile,"address":$scope.address,"emergency":$scope.emergency};
 
-                 $http({
-                     method:'POST',
-                     url:'/addNew',
-                     data: JSON.stringify(data1),
-                     contentType: 'application/json',
-                     dataType:'json'
-                 })
-        }})
 
 
